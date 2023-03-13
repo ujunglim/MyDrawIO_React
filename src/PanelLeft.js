2da5007b507a925d0194d7b57700b2e9d0b0a806
v_linyouzhen <v_linyouzhen@baidu.com>
2023-03-13 20:30:25 +0800

    add typing title effect

--- src/PanelLeft.js
+++ src/PanelLeft.js
@@ -1,14 +1,60 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { Button, Input } from "@ui5/webcomponents-react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DrawControllerInstance from "./Controller/DrawController";
 
 function PanelLeft() {
+  const completionWord = "Hi I'm Summer";
+  const [title, setTitle] = useState();
+  const indexRef = useRef(0);
+  const isTyping = useRef(true);
+
+  const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setTitle(async (prevTitle) => {
+        if (isTyping.current) {
+          // typing
+          const result = prevTitle ? prevTitle + completionWord[indexRef.current] : completionWord[0];
+          indexRef.current++;
+          // finish typing
+          if (indexRef.current + 1 > completionWord.length) {
+            // wait
+            await sleep(3000);
+            isTyping.current = false;
+          }
+          return result;
+        }
+        else {
+          // delete
+          const prevTitleArr = prevTitle.split('');
+
+          // finish deleting
+          if (prevTitleArr.length === 0) {
+            // wait
+            isTyping.current = true;
+            indexRef.current = 0;
+          }
+          // remove
+          prevTitleArr.pop();
+          return prevTitleArr.join('');
+        }
+
+      })
+    }, 300);
+
+    return () => clearInterval(timeInterval);
+  }, []);
+
   const handleCreateRect = () => {
     DrawControllerInstance.rectManager.createRect();
   };
   return (
     <div className="panel">
+      <h1 className="title">{title}</h1>
       <Input type="text" placeholder="Search Shapes" />
       <img
         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIGNIUk0AAHolAACAgwAA+f8AAIDpAAB1MAAA6mAAADqYAAAXb5JfxUYAAAEaSURBVHjabNGxS5VxFIfxz71XaWuQUJCG/gCHhgTD9VpEETg4aMOlQRp0EoezObgcd220KQiXmpretTAHQRBdojlQEJyukPdt+b1ywfvAGc7wnHP4nlZd1yKijQW8xzNc4Su+ZOYfQ3T6/f4YNvEJYzjELXp4VVXVz263+7cR2niBxAFeZ2YPi3iHR/gYERPDwhpOsd6sz8x/mfkNG3iOlWFhFj8y89J9KvzGXER0GuEaD42mgwHqUtoljbcRsTBCeINpfM/MgZLKPpaxFxGbOCqDXmILN7hoJrTKH+axhxmcYRxP0MIDnOBDZv5q1XUNIuJxifJp+UNV7t7BFM6xeic0RMQ4Bpl5W/ol7GISx/eEUUTECrbx+f8A8xhiZht9zsgAAAAASUVORK5CYII="
